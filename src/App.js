9bdb1ca099582ba529628987a32cf813b893c22a
Haneefe <haneefalikhan18@gmail.com>
2023-02-03 00:26:03 +0530

    Team section completed

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import TopBar from "./scenes/global/topBar";
 import DashBoard from "./scenes/dashboard";
 import SideBar from "./scenes/global/sideBar";
 import Team from "./scenes/team";
-// import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
@@ -27,8 +27,10 @@ function App() {
             <Routes>
               <Route path="/" element={<DashBoard />} />
               <Route path="/team" element={<Team />} />
-              {/*
               <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              {/*
+              
               <Route path="/bar" element={<Bar />} />
               <Route path="/form" element={<Form />} />
               <Route path="/line" element={<Line />} />
